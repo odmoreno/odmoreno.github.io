6804e1eb0ca193a95dc0ce3426f9f835c9f5322a
CarlosMH95 <cmanosalvas95@gmail.com>
2016-07-05 17:25:53 -0500

    Agregando funcionalidades a operarios

--- js/ingresarOp.js
+++ js/ingresarOp.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
 	 $('#opcion1 a').click(function(event) {
-	 event.preventDefault();
-	 $('#datosOperario').load('registrar.html');
+		event.preventDefault();
+		$('#datosOperario').load('registrar.html');
 	 });
+	 $('#opcion2 a').click(function(event) {
+		event.preventDefault();
+		$('#datosOperario').load('eliminarMod.html');
+		var xhttp= new XMLHttpRequest();
+		xhttp.onreadystatechange = function(){
+			if (xhttp.readyState == 4 && xhttp.status == 200){
+				
+				var muestras=document.getElementById("muestras");
+				var json = JSON.parse(xhttp.responseText);
+				json.forEach(function(dato){
+					var option= document.createElement("option");
+					var numMuestra=dato.numero;
+					option.textContent(numMuestra);
+					muestras.appendChild(option);
+				});
+			}
+		}
+		xhttp.open("GET","json/datosOperario.json", true);
+	xhttp.send();
+	});
 });
